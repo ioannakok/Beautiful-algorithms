25ff04175cf1a1fde4b2f915c8e87ea671b37805
ioannakok <ioannakok@hotmail.com>
2017-09-02 18:19:45 +0100

    Fixed server error

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
+/* https://www.youtube.com/watch?v=DQ9pZ2NKXRo 
+   https://www.youtube.com/watch?v=OILrJmjkId4	
+*/
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
-const mongoose = require('mongoose');
-const config = require('./config/database');
-
-// Database connection
-mongoose.connect(config.database);
-
-// On Connection
-mongoose.connection.on('connected', () => {
-	console.log('Connected to database ' + config.database);
-});
-
-// On Error
-mongoose.connection.on('error', (err) => {
-	console.log('Database error: ' + err);
-});
-
 
 const app = express();
-
-const algorithms = require('./routes/app');
 
 // Port Number
 const port = 3000;
@@ -30,21 +16,24 @@ const port = 3000;
 // CORS Middleware
 app.use(cors());
 
-// Set Static Folder
+// Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
-
-app.use('/index', algorithms);
 
 // Index Route
 app.get('/', (req, res) => {
 	res.send('Beautiful Algorithms Project');
 });
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 // Start Server
 app.listen(port, () => {
-	console.log('Server started on port ' + 3000);
+	console.log('Server started on port ' + port);
 });
+
 
